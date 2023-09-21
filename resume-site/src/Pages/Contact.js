9b09cb6397287e279aa9cb87940c35ba0a0b3ebe
ardacey <ac.ardaceylan@gmail.com>
2023-09-21 22:36:02 +0300

    Update Contact.js

--- resume-site/src/Pages/Contact.js
+++ resume-site/src/Pages/Contact.js
@@ -3,53 +3,38 @@ import translations from "../Components/Translations";
 
 export default function Contact() {
     const [selectedLanguage, setSelectedLanguage] = useState(localStorage.getItem('selectedLanguage'));
-    const [emailData, setEmailData] = useState({
-        name: '',
-        email: '',
-        message: '',
-      });
     
     useEffect(() => {
       setSelectedLanguage(localStorage.getItem('selectedLanguage'));
     }, []);
     const translation = translations[selectedLanguage];
-
-    const handleChange = (e) => {
-        setEmailData({ ...emailData, [e.target.name]: e.target.value });
-      };
     
     return (
         <div className="contact">
             <img src="contact-img.jpg" className='contact-img' alt="Arda Ceylan at the waterfall"></img>
-            <form className='contact-form' method="POST" data-netlify="true" netlify>
+            <form name='contactform' className='contact-form' method="POST" data-netlify="true">
                 <h1>{translation.contact.contactme}</h1>
                 <div className="form-row">
                     <input 
                     className="form-input" 
                     type="text" 
                     name="name" 
-                    value={emailData.name} 
                     placeholder={translation.contact.name}
-                    onChange={handleChange}
                     required></input>
                 </div>
                 <div className="form-row">
                     <input 
                     className="form-input" 
                     type="email" 
-                    name="email" 
-                    value={emailData.email} 
+                    name="email"  
                     placeholder={translation.contact.email} 
-                    onChange={handleChange}
                     required></input>
                 </div>
                 <div className="form-row">
                     <textarea 
                     className="form-input message" 
                     name="message" 
-                    value={emailData.message} 
                     placeholder={translation.contact.message}
-                    onChange={handleChange}
                     required></textarea>
                 </div>
                 <button className="submit" type="submit">{translation.contact.submit}</button>
