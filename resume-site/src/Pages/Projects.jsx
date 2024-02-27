06ba2ac18f60a02ccab97ec0176d7de45eeb6e45
ardacey <ac.ardaceylan@gmail.com>
2024-02-27 21:36:34 +0300

    new page added

--- resume-site/src/Pages/Projects.jsx
+++ resume-site/src/Pages/Projects.jsx
@@ -10,8 +10,45 @@ export default function Projects() {
     const translation = translations[selectedLanguage];
 
     return (
-        <div>
-            work in progress
+        <div style={{
+            height: '300vh',
+        }}>
+            <div className='project-content'>
+                <iframe 
+                className='gif1'
+                src='giphy.gif' >
+                </iframe>
+                <p className='project-text1'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pharetra sapien et eleifend interdum. Nunc tristique rhoncus justo nec ultricies. Cras placerat finibus odio sit amet auctor. Donec ex nulla, efficitur vitae diam sit amet, dictum convallis magna. Maecenas eget felis viverra est convallis aliquet id in arcu. Proin id sodales nisl. Aenean quis scelerisque lacus, in rhoncus neque. Sed dui nibh, tempor ac leo ut, consequat blandit mi. Nulla facilisi. Nunc fermentum tellus sit amet turpis porta, eu mattis nunc rhoncus. Nam cursus condimentum egestas. Proin fermentum dignissim lorem vel auctor. Pellentesque condimentum, velit sed semper commodo, sapien odio aliquam odio, eu facilisis ipsum sapien ac arcu. Morbi efficitur ultrices dui, semper consequat velit dapibus at.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pharetra sapien et eleifend interdum. Nunc tristique rhoncus justo nec ultricies. Cras placerat finibus odio sit amet auctor. Donec ex nulla, efficitur vitae diam sit amet, dictum convallis magna.
+                </p>
+            </div>
+            <div className='project-content'>
+                <iframe 
+                className='gif2'
+                src='giphy.gif' >
+                </iframe>
+                <p className='project-text2'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pharetra sapien et eleifend interdum. Nunc tristique rhoncus justo nec ultricies. Cras placerat finibus odio sit amet auctor. Donec ex nulla, efficitur vitae diam sit amet, dictum convallis magna. Maecenas eget felis viverra est convallis aliquet id in arcu. Proin id sodales nisl. Aenean quis scelerisque lacus, in rhoncus neque. Sed dui nibh, tempor ac leo ut, consequat blandit mi. Nulla facilisi. Nunc fermentum tellus sit amet turpis porta, eu mattis nunc rhoncus. Nam cursus condimentum egestas. Proin fermentum dignissim lorem vel auctor. Pellentesque condimentum, velit sed semper commodo, sapien odio aliquam odio, eu facilisis ipsum sapien ac arcu. Morbi efficitur ultrices dui, semper consequat velit dapibus at.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pharetra sapien et eleifend interdum. Nunc tristique rhoncus justo nec ultricies. Cras placerat finibus odio sit amet auctor. Donec ex nulla, efficitur vitae diam sit amet, dictum convallis magna.
+                </p>
+            </div>
+            <div className='project-content'>
+                <iframe 
+                className='gif1'
+                src='giphy.gif' >
+                </iframe>
+                <p className='project-text1'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pharetra sapien et eleifend interdum. Nunc tristique rhoncus justo nec ultricies. Cras placerat finibus odio sit amet auctor. Donec ex nulla, efficitur vitae diam sit amet, dictum convallis magna. Maecenas eget felis viverra est convallis aliquet id in arcu. Proin id sodales nisl. Aenean quis scelerisque lacus, in rhoncus neque. Sed dui nibh, tempor ac leo ut, consequat blandit mi. Nulla facilisi. Nunc fermentum tellus sit amet turpis porta, eu mattis nunc rhoncus. Nam cursus condimentum egestas. Proin fermentum dignissim lorem vel auctor. Pellentesque condimentum, velit sed semper commodo, sapien odio aliquam odio, eu facilisis ipsum sapien ac arcu. Morbi efficitur ultrices dui, semper consequat velit dapibus at.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pharetra sapien et eleifend interdum. Nunc tristique rhoncus justo nec ultricies. Cras placerat finibus odio sit amet auctor. Donec ex nulla, efficitur vitae diam sit amet, dictum convallis magna.
+                </p>
+            </div>
+            <div className='project-content'>
+                <iframe 
+                className='gif2'
+                src='giphy.gif' >
+                </iframe>
+                <p className='project-text2'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pharetra sapien et eleifend interdum. Nunc tristique rhoncus justo nec ultricies. Cras placerat finibus odio sit amet auctor. Donec ex nulla, efficitur vitae diam sit amet, dictum convallis magna. Maecenas eget felis viverra est convallis aliquet id in arcu. Proin id sodales nisl. Aenean quis scelerisque lacus, in rhoncus neque. Sed dui nibh, tempor ac leo ut, consequat blandit mi. Nulla facilisi. Nunc fermentum tellus sit amet turpis porta, eu mattis nunc rhoncus. Nam cursus condimentum egestas. Proin fermentum dignissim lorem vel auctor. Pellentesque condimentum, velit sed semper commodo, sapien odio aliquam odio, eu facilisis ipsum sapien ac arcu. Morbi efficitur ultrices dui, semper consequat velit dapibus at.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pharetra sapien et eleifend interdum. Nunc tristique rhoncus justo nec ultricies. Cras placerat finibus odio sit amet auctor. Donec ex nulla, efficitur vitae diam sit amet, dictum convallis magna.
+                </p>
+            </div>
         </div>
     )
 }
