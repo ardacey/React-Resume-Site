9877b65f42015aed6255cfbf9ef0fa9daa79b993
ardacey <ac.ardaceylan@gmail.com>
2024-03-05 15:11:55 +0300

    new page added

--- resume-site/src/Pages/Projects.jsx
+++ resume-site/src/Pages/Projects.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import translations from "../Components/Translations";
+import { NavLink} from "react-router-dom";
 
 export default function Projects() {
     const [selectedLanguage, setSelectedLanguage] = useState(localStorage.getItem('selectedLanguage'));
@@ -10,44 +11,94 @@ export default function Projects() {
     const translation = translations[selectedLanguage];
 
     return (
-        <div style={{
-            height: '300vh',
-        }}>
+        <div className='projects'>
             <div className='project-content'>
-                <iframe 
+                <img
                 className='gif1'
-                src='giphy.gif' >
-                </iframe>
-                <p className='project-text1'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pharetra sapien et eleifend interdum. Nunc tristique rhoncus justo nec ultricies. Cras placerat finibus odio sit amet auctor. Donec ex nulla, efficitur vitae diam sit amet, dictum convallis magna. Maecenas eget felis viverra est convallis aliquet id in arcu. Proin id sodales nisl. Aenean quis scelerisque lacus, in rhoncus neque. Sed dui nibh, tempor ac leo ut, consequat blandit mi. Nulla facilisi. Nunc fermentum tellus sit amet turpis porta, eu mattis nunc rhoncus. Nam cursus condimentum egestas. Proin fermentum dignissim lorem vel auctor. Pellentesque condimentum, velit sed semper commodo, sapien odio aliquam odio, eu facilisis ipsum sapien ac arcu. Morbi efficitur ultrices dui, semper consequat velit dapibus at.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                src='blockfall.gif' >
+                </img>
+                <p className='project-title1'>
+                    {translation.projects.blockfall.title}
                 </p>
+                <ul className='project-text1'>
+                    <li>{translation.projects.blockfall.tech}</li>
+                    <li>{translation.projects.blockfall.desc}</li>
+                    <li>{translation.projects.blockfall.link}
+                    <NavLink 
+                        to = "https://github.com/ardacey/BlockFall-Tetris" 
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                        style={{color: 'aliceblue'}}>
+                        {translation.projects.blockfall.title}
+                    </NavLink>
+                    </li>
+                </ul>
             </div>
             <div className='project-content'>
-                <iframe 
+                <img 
                 className='gif2'
-                src='giphy.gif' >
-                </iframe>
-                <p className='project-text2'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pharetra sapien et eleifend interdum. Nunc tristique rhoncus justo nec ultricies. Cras placerat finibus odio sit amet auctor. Donec ex nulla, efficitur vitae diam sit amet, dictum convallis magna. Maecenas eget felis viverra est convallis aliquet id in arcu. Proin id sodales nisl. Aenean quis scelerisque lacus, in rhoncus neque. Sed dui nibh, tempor ac leo ut, consequat blandit mi. Nulla facilisi. Nunc fermentum tellus sit amet turpis porta, eu mattis nunc rhoncus. Nam cursus condimentum egestas. Proin fermentum dignissim lorem vel auctor. Pellentesque condimentum, velit sed semper commodo, sapien odio aliquam odio, eu facilisis ipsum sapien ac arcu. Morbi efficitur ultrices dui, semper consequat velit dapibus at.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                src='duckhunt.gif' >
+                </img>
+                <p className='project-title2'>
+                    {translation.projects.duckhunt.title}
                 </p>
+                <ul className='project-text2'>
+                    <li>{translation.projects.duckhunt.tech}</li>
+                    <li>{translation.projects.duckhunt.desc}</li>
+                    <li>{translation.projects.duckhunt.link}
+                    <NavLink 
+                        to = "https://github.com/ardacey/Java-Duck-Hunt" 
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                        style={{color: 'aliceblue'}}>
+                        {translation.projects.duckhunt.title}
+                    </NavLink>
+                    </li>
+                </ul>
             </div>
             <div className='project-content'>
-                <iframe 
+                <img 
                 className='gif1'
-                src='giphy.gif' >
-                </iframe>
-                <p className='project-text1'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pharetra sapien et eleifend interdum. Nunc tristique rhoncus justo nec ultricies. Cras placerat finibus odio sit amet auctor. Donec ex nulla, efficitur vitae diam sit amet, dictum convallis magna. Maecenas eget felis viverra est convallis aliquet id in arcu. Proin id sodales nisl. Aenean quis scelerisque lacus, in rhoncus neque. Sed dui nibh, tempor ac leo ut, consequat blandit mi. Nulla facilisi. Nunc fermentum tellus sit amet turpis porta, eu mattis nunc rhoncus. Nam cursus condimentum egestas. Proin fermentum dignissim lorem vel auctor. Pellentesque condimentum, velit sed semper commodo, sapien odio aliquam odio, eu facilisis ipsum sapien ac arcu. Morbi efficitur ultrices dui, semper consequat velit dapibus at.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                src='knn.webp' >
+                </img>
+                <p className='project-title1'>
+                    {translation.projects.knn.title}
                 </p>
+                <ul className='project-text1'>
+                    <li>{translation.projects.knn.tech}</li>
+                    <li>{translation.projects.knn.desc}</li>
+                    <li>{translation.projects.knn.link}
+                    <NavLink 
+                        to = "https://github.com/ardacey/Personality-Classification-With-KNN-Algorithm" 
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                        style={{color: 'aliceblue'}}>
+                        {translation.projects.knn.title}
+                    </NavLink>
+                    </li>
+                </ul>
             </div>
             <div className='project-content'>
-                <iframe 
+                <img 
                 className='gif2'
-                src='giphy.gif' >
-                </iframe>
-                <p className='project-text2'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pharetra sapien et eleifend interdum. Nunc tristique rhoncus justo nec ultricies. Cras placerat finibus odio sit amet auctor. Donec ex nulla, efficitur vitae diam sit amet, dictum convallis magna. Maecenas eget felis viverra est convallis aliquet id in arcu. Proin id sodales nisl. Aenean quis scelerisque lacus, in rhoncus neque. Sed dui nibh, tempor ac leo ut, consequat blandit mi. Nulla facilisi. Nunc fermentum tellus sit amet turpis porta, eu mattis nunc rhoncus. Nam cursus condimentum egestas. Proin fermentum dignissim lorem vel auctor. Pellentesque condimentum, velit sed semper commodo, sapien odio aliquam odio, eu facilisis ipsum sapien ac arcu. Morbi efficitur ultrices dui, semper consequat velit dapibus at.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                src='weather.webp' >
+                </img>
+                <p className='project-title2'>
+                    {translation.projects.weather.title}
                 </p>
+                <ul className='project-text2'>
+                    <li>{translation.projects.weather.tech}</li>
+                    <li>{translation.projects.weather.desc}</li>
+                    <li>{translation.projects.weather.link}
+                    <NavLink 
+                        to = "https://hacettepe-weather.vercel.app/" 
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                        style={{color: 'aliceblue'}}>
+                        {translation.projects.weather.title}
+                    </NavLink>
+                    </li>
+                </ul>
             </div>
         </div>
     )
